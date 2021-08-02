ad7d09a164cc329a986604c0ed69ec3dd1b7e768
Alexey <wrck7@yandex.ru>
2021-08-02 11:09:41 +0300

    Добавлена проверка заполнения свойств элемента при выводе их через news.list

--- bitrix/templates/eshop_bootstrap_v4/components/bitrix/news/blog/bitrix/news.list/.default/template.php
+++ bitrix/templates/eshop_bootstrap_v4/components/bitrix/news/blog/bitrix/news.list/.default/template.php
@@ -7,17 +7,25 @@
             <?$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem['IBLOCK_ID'], 'ELEMENT_DELETE'))?>
             <div class="col-blog" ontouchstart="this.classList.toggle('hover');">
                 <a class="container-blog" href="<?=$arItem['DETAIL_PAGE_URL']?>" id="<?=$this->GetEditAreaId($arItem['ID'])?>">
-                    <div class="front" style="background-image: url('<?=CFile::GetPath($arItem["PREVIEW_PICTURE"]["ID"])?>')">
+                    <?if($arItem['PREVIEW_PICTURE']):?>
+                    <div class="front" style="background-image: url('<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>')">
                         <div class="inner">
+                            <?if($arItem['NAME']):?>
                             <p><?=$arItem['NAME'] ?></p>
+                            <?endif?>
+                            <?if($arItem['PREVIEW_TEXT']):?>
                             <span><?=$arItem['PREVIEW_TEXT'] ?></span>
+                            <?endif?>
                         </div>
                     </div>
+                    <?endif?>
+                    <?if($arItem['DETAIL_TEXT']):?>
                     <div class="back">
                         <div class="inner">
                             <p><?=$arItem['DETAIL_TEXT'] ?></p>
                         </div>
                     </div>
+                    <?endif?>
                 </a>
             </div>
         <?endforeach?>
